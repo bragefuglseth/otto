f1f024f140d54760842b327ce3d8f0110188e6ea
Brage Fuglseth <brage.fuglseth@gmail.com>
2022-01-29 22:08:04 +0100

    Add support for slash commands

--- src/app.js
+++ src/app.js
@@ -1,9 +1,43 @@
 require("dotenv").config();
-const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, Intents } = require("discord.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Get all files within command directory
+const commandDir = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandDir)
+  .filter(file => file.endsWith(".js"));
+
+// Add all JS files to a collection
+client.commands = new Collection();
+for (file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
 client.once("ready", () => {
   console.log("Client is ready");
 });
 
+client.on("interactionCreate", async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command.",
+      ephemeral: true
+    });
+  }
+});
+
+// Login with token from .env file
 client.login(process.env.DISCORD_TOKEN);
